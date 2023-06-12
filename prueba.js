8b0e8f4f38c278d0c2ebc0ed5371654f8598e833
CristianP1 <perezcristian730@gmail.com>
2023-06-12 10:46:36 -0500

    primer js en git

---
+++ prueba.js
@@ -0,0 +1,27 @@
+//RECORDANDO FUNCIONES
+
+/*
+function test_js(name, lastname, nickname) {
+    let completeName = name + " " + lastname
+    return `Mi nombre completo es:  ${completeName}, pero prefiero que me digan ${nickname}.`
+}
+
+
+console.log(test_js("Cristian", "Sanchez", "CristianP1"))
+*/
+
+
+// RECORDANDO CONDICIONALES
+
+let array_a = [
+    {type: "Free", description: "Solo puedes tomar los cursos gratis"},
+    {type: "Basic", description: "Puedes tomar casi todos los cursos de Platzi durante un mes"},
+    {type: "Expert", description: "Puedes tomar casi todos los cursos de Platzi durante un año"},
+    {type: "ExpertPlus", description: "Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año"}
+]
+
+for (let i = 0; i < array_a.length; i++) {
+    if (array_a[i].type == "Free") {
+        console.log(array_a[i].description)
+    }
+}
